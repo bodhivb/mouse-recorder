330005645930457824f6fa7cd3d5f354132e06fc
Bodhi van Baardewijk <bodhivanbaardewijk@hotmail.com>
2019-10-05 15:16:46 +0200

    Add basic keyboard hook

---
+++ KeyboardHook.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace MouseRecorder
+{
+    class KeyboardHook
+    {
+        private int hookId;
+        public WinUserDll.HookProc hookProc;
+
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+
+        public void SetHook()
+        {
+            hookProc = new WinUserDll.HookProc(KeyboardHookProc);
+            hookId = WinUserDll.SetWindowsHookEx(WH_KEYBOARD_LL, hookProc, IntPtr.Zero, 0);
+        }
+
+        public void UnHook()
+        {
+            WinUserDll.UnhookWindowsHookEx(hookId);
+        }
+
+
+        private int KeyboardHookProc (int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (wParam == (IntPtr) WM_KEYDOWN)
+            {
+                MessageBox.Show("key pressed " + (Keys)Marshal.ReadInt32(lParam));
+            }
+            return WinUserDll.CallNextHookEx(hookId, nCode, wParam, lParam);
+        }
+    }
+}
